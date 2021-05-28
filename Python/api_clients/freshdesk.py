b0c042339001275df4da5ec55faa45e3dc16e1a1
Georgi Gamzakov <ggamzakov@gmail.com>
2021-05-28 18:11:09 +0300

    python base task without tests

---
+++ Python/api_clients/freshdesk.py
@@ -0,0 +1,60 @@
+import requests
+from requests.auth import HTTPBasicAuth
+from loggers.console import ConsoleLogger
+
+logger = ConsoleLogger()
+
+class FreshdeskException(Exception):
+    pass
+
+class FreshdeskClient:
+    def __init__(self, domain, access_token):
+        self.base_url = "https://{}.freshdesk.com/api/v2".format(domain)
+        self.access_token = access_token
+        self.session = requests.Session()
+        self.session.auth = HTTPBasicAuth(self.access_token,"X")
+    
+    def get_contact_id_by_external_id(self, external_id):
+        response = self.session.get("{}/search/contacts?query=\"unique_external_id:'{}'\"".format(self.base_url, external_id))
+        response.raise_for_status()
+        res_json = response.json()
+
+        if res_json.get("total", 0) == 0:
+            return None
+        
+        return res_json['results'][0]['id']
+
+    def create_or_update_contact(self, user):
+        if not user.username:
+            raise FreshdeskException("Username missing in parameter user!")
+
+        #Name is mandatory acording to Freshdesk api documentation
+        if not user.name:
+            raise FreshdeskException("Name missing in parameter user!")
+
+        contact_id = self.get_contact_id_by_external_id(user.username)
+        payload = self.payload_from_user(user)
+
+        if not contact_id:
+            #create contact
+            response = self.session.post("{}/contacts".format(self.base_url), json=payload)
+        else:
+            #update contact
+            response = self.session.put("{}/contacts/{}".format(self.base_url, contact_id), json=payload)
+        
+        response.raise_for_status()
+        
+        if not contact_id:
+            logger.log_info("Freshdesk contact created successfully")
+        else:
+            logger.log_info("Freshdesk contact updated successfully")  
+
+    def payload_from_user(self, user):
+        return {
+            "unique_external_id": user.username,
+            "name": user.name,
+            "email": user.email,
+            "address": user.location,
+            "twitter_id": user.twitter_username,
+            "description": user.bio
+        }
--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,49 @@
-msg = "Hello World"
-print(msg)
+import json
+import os
+import sys
+
+from requests.exceptions import HTTPError
+from api_clients.github import GithubClient
+from api_clients.freshdesk import FreshdeskClient, FreshdeskException
+from loggers.console import ConsoleLogger
+
+logger = ConsoleLogger()
+
+github_user_name = os.getenv("GITHUB_USERNAME")
+github_access_token = os.getenv("GITHUB_TOKEN")
+freshdesk_domain = os.getenv("FRESHDESK_DOMAIN")
+freshdesk_access_token = os.getenv("FRESHDESK_TOKEN")
+
+if github_user_name is None:
+    logger.log_error("Missing environment variable 'GITHUB_USERNAME'! Exiting!")
+    sys.exit()
+
+if github_access_token is None:
+    logger.log_error("Missing environment variable 'GITHUB_TOKEN'! Exiting!")
+    sys.exit()
+
+if freshdesk_domain is None:
+    logger.log_error("Missing environment variable 'FRESHDESK_DOMAIN'! Exiting!")
+    sys.exit()
+
+if freshdesk_access_token is None:
+    logger.log_error("Missing environment variable 'FRESHDESK_TOKEN'! Exiting!")
+    sys.exit()
+
+github_client = GithubClient(user_name=github_user_name, access_token=github_access_token)#"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")
+
+try:
+    user_info = github_client.get_user_info()
+except HTTPError as ex:
+    logger.log_error("Github get user info request failed with: Status code: {} Reason: {}".format(ex.response.status_code, ex.response.reason))
+
+freshdesk_client = FreshdeskClient(domain=freshdesk_domain, access_token=freshdesk_access_token)
+
+try:
+    freshdesk_client.create_or_update_contact(user_info)
+except HTTPError as ex:
+    logger.log_error("Freshdesk create/update contact failed with: Status code: {} Reason: {}".format(ex.response.status_code, ex.response.reason))
+except FreshdeskException as ex:
+    logger.log_error("Freshdesk create/update contact failed with: {}".format(str(ex)))
+
+#print(test2.login)
